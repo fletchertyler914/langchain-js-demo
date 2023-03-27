f814e8adfcbc3825af8823ed4c7eab2c16b7c0e0
Tyler Fletcher <fletchertyler914@yahoo.com>
2023-03-26 22:41:01 -0500

    demo base

--- apps/site/pages/_app.tsx
+++ apps/site/pages/_app.tsx
@@ -7,6 +7,14 @@ function CustomApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Welcome to site!</title>
+
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Site description" />
+        <meta name="keywords" content="site, nextjs, react" />
+        <meta name="author" content="Site author" />
+
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="app">
         <Component {...pageProps} />
